16dd0ff54dedf5b0439250dbc730cf4e4c1ef479
Jon Freer <freerjm@miamioh.edu>
2018-07-26 00:09:36 -0700

    Introduce package javadoc for api.representations package.

---
+++ src/main/java/api/representations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains the constructs that together abstractly define a 
+ * RESTful resource representation, as well as the abstract types
+ * responsible for creating representations.
+ */
+package api.representations;
