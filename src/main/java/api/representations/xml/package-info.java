076cce26605b5b9eaba1cc019502b7a8c9b7bb3b
Jon Freer <freerjm@miamioh.edu>
2018-07-26 00:08:33 -0700

    Introduce package javadoc for api.representations.xml package.

---
+++ src/main/java/api/representations/xml/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains the constructs that serve as RESTful resource representations that are serialized and
+ * deserialized as the {@code application/xml} media type, as well as the types responsible for
+ * creating these representations.
+ */
+package api.representations.xml;
