8f8bcd8b09078b882252e937614960c628df04fd
Jon Freer <freerjm@miamioh.edu>
2018-07-26 00:04:21 -0700

    Introduce javadoc for notification XML representation.

--- src/main/java/api/representations/xml/Notification.java
+++ src/main/java/api/representations/xml/Notification.java
@@ -11,7 +11,7 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- * Defines the 'application/xml' representation of a Notification resource.
+ * Defines the {@code application/xml} representation of a Notification resource.
  *
  * @author Jon Freer
  */
@@ -26,7 +26,7 @@ public final class Notification extends Representation {
   private Set<Target> targets;
   private Set<Audience> audiences;
 
-  /** Constructs an empty instance of {@link Notification}. */
+  /** Constructs a new {@link Notification} representation. */
   public Notification() {
     super(MediaType.APPLICATION_XML_TYPE);
 
@@ -71,6 +71,11 @@ public final class Notification extends Representation {
     this.audiences = audiences == null ? new HashSet<>() : audiences;
   }
 
+  /**
+   * A builder of {@link Notification} instances.
+   *
+   * @author Jon Freer
+   */
   public static final class Builder extends Representation.Builder {
 
     private UUID uuid;
@@ -81,57 +86,128 @@ public final class Notification extends Representation {
     private Set<Target> targets;
     private Set<Audience> audiences;
 
+    /** Constructs a builder of {@link Notification} instances. */
     public Builder() {
       super(MediaType.APPLICATION_XML_TYPE);
       this.targets = new HashSet<>();
       this.audiences = new HashSet<>();
     }
 
+    /**
+     * Sets the universally unique identifier of the {@link Notification} representation being
+     * built.
+     *
+     * @param uuid The desired {@link UUID} of the {@link Notification} representation being built.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder uuid(UUID uuid) {
       this.uuid = uuid;
       return this;
     }
 
+    /**
+     * Sets the textual content of the {@link Notification} representation being built.
+     *
+     * @param content The desired textual content of the {@link Notification} representation being
+     *     built.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder content(String content) {
       this.content = content;
       return this;
     }
 
+    /**
+     * Sets the date and time of the {@link Notification} representation that indicates when the
+     * notification was sent. A value of {@code null} indicates the notification has not yet been
+     * sent.
+     *
+     * @param sentAt The desired date and time of the {@link Notification} representation indicating
+     *     when the notification was sent.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder sentAt(Date sentAt) {
       this.sentAt = sentAt;
       return this;
     }
 
+    /**
+     * Sets the date and time of the {@link Notification} representation that indicates when the
+     * notification should be sent.
+     *
+     * @param sendAt The desired date and time of the {@link Notification} representation indicating
+     *     when the notification should be sent.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder sendAt(Date sendAt) {
       this.sendAt = sendAt;
       return this;
     }
 
+    /**
+     * Sets the status of the {@link Notification} representation being built.
+     *
+     * @param status The desired status of the {@link Notification} representation bing built.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder status(NotificationStatus status) {
       this.status = status;
       return this;
     }
 
+    /**
+     * Appends the provided {@link Target} to the list of targets for the {@link Notification}
+     * representation.
+     *
+     * @param target The {@link Target} to append to the list of targets for the {@link
+     *     Notification} representation.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder addTarget(Target target) {
       this.targets.add(target);
       return this;
     }
 
+    /**
+     * Sets the list of targets for the {@link Notification} representation.
+     *
+     * @param targets The desired list of targets for the {@link Notification} representation.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder targets(Set<Target> targets) {
       this.targets = targets;
       return this;
     }
 
+    /**
+     * Appends the provided {@link Audience} to the list of audiences for the {@link Notification}
+     * representation.
+     *
+     * @param audience The {@link Audience} to append to the list of audiences for the {@link
+     *     Notification} representation.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder addAudience(Audience audience) {
       this.audiences.add(audience);
       return this;
     }
 
+    /**
+     * Sets the list of audiences for the {@link Notification} representation.
+     *
+     * @param audiences The desired list of audiences for the {@link Notification} representation.
+     * @return The updated {@link Notification} builder.
+     */
     public Builder audiences(Set<Audience> audiences) {
       this.audiences = audiences;
       return this;
     }
 
+    /**
+     * Builds the {@link Notification} instance.
+     *
+     * @return The {@link Notification} instance.
+     */
     @Override
     public Representation build() {
       Notification n = new Notification();
@@ -150,83 +226,116 @@ public final class Notification extends Representation {
   }
 
   /**
-   * Retrieves the universally unique identifier of this notification.
+   * Retrieves the universally unique identifier of this notification representation.
    *
-   * @return The universally unique identifier of this notification.
+   * @return The universally unique identifier of this notification representation.
    */
   @XmlElement
   public UUID getUUID() {
     return this.uuid;
   }
 
+  /**
+   * Alters the universally unique identifier of this notification representation.
+   *
+   * @param uuid The desired universally unique identifier of the notification representation.
+   */
   private void setUUID(UUID uuid) {
     this.uuid = uuid;
   }
 
   /**
-   * Retrieves the information being communicated within this notification.
+   * Retrieves the textual information being communicated within this notification representation.
    *
-   * @return The information being communicated within this notification.
+   * @return The textual information being communicated within this notification representation.
    */
   @XmlElement
   public String getContent() {
     return this.content;
   }
 
+  /**
+   * Alters the information being communicated within this notification representation.
+   *
+   * @param content The desired information being communicated within this notification
+   *     representation.
+   */
   private void setContent(String content) {
     this.content = content;
   }
 
   /**
-   * Retrieves the status the delivery status of this notification.
+   * Retrieves the delivery status of this notification representation.
    *
-   * @return The status of the notification in terms of its delivery to its audiences and targets.
+   * @return The delivery status of the notification representation.
    */
   @XmlElement
   public NotificationStatus getStatus() {
     return this.status;
   }
 
+  /**
+   * Alters the delivery status of the notification representation.
+   *
+   * @param status The desired status of the notification representation.
+   */
   private void setStatus(NotificationStatus status) {
     this.status = status;
   }
 
   /**
-   * Retrieves the date and time in which this notification is to be sent. A value of {@code null}
-   * indicates that the notification should be sent immediately.
+   * Retrieves the date and time of the notification representation indicating when the notification
+   * should be sent. A value of {@code null} indicates that the notification should be sent
+   * immediately.
    *
-   * @return States when the notification should be sent to its targets and audiences.
+   * @return The date and time of the notification representation indicating when the notification
+   *     should be sent.
    */
   @XmlElement
   public Date getSendAt() {
     return this.sendAt;
   }
 
+  /**
+   * Alters the date and time of the notification representation indicating when the notification
+   * should be sent.
+   *
+   * @param sendAt The desired date and time of the notification representation indicating when the
+   *     notification should be sent.
+   */
   private void setSendAt(Date sendAt) {
     this.sendAt = sendAt;
   }
 
   /**
-   * Retrieves the date and time in which this notification was sent to all of its targets and
-   * audiences. A value of {@code null} indicates that the notification has not been sent to all of
-   * its targets and audiences. Please refer to {@link api.representations.xml.NotificationStatus}
-   * to gain more insight into the progress of the notification delivery.
+   * Retrieves the date and time of the notification representation indicating when the notification
+   * was sent to all of its targets and audiences.
    *
-   * @return States when the notification was sent to all of its targets and all of its audiences.
+   * @return The date and time of the notification representation indicating when the notification
+   *     was sent to all of its targets and all of its audiences.
    */
   @XmlElement
   public Date getSentAt() {
     return this.sentAt;
   }
 
+  /**
+   * Alters the date and time of the notification representation indicating when the notification
+   * should be sent.
+   *
+   * @param sendAt The desired date and time of the notification representation indicating when the
+   *     notification should be sent.
+   */
   private void setSentAt(Date sentAt) {
     this.sentAt = sentAt;
   }
 
   /**
-   * Retrieves the explicit recipients that should receive this notification.
+   * Retrieves the list of targets of the notification representation that identify the explicit
+   * individual recipients that should receive this notification.
    *
-   * @return Explicit recipients that should receive this notification.
+   * @return The list of targets of the notification representation that identify the explicit
+   *     recipients that should receive this notification.
    */
   @XmlElementWrapper(name = "targets")
   @XmlElement(name = "target")
@@ -234,14 +343,23 @@ public final class Notification extends Representation {
     return this.targets;
   }
 
+  /**
+   * Alters the list of targets of the notification representation that identify the explicit
+   * individual recipients that should receive this notification.
+   *
+   * @return The list of targets of the notification representation that identify the explicit
+   *     recipients that should receive this notification.
+   */
   private void setTargets(Set<Target> targets) {
     this.targets = targets;
   }
 
   /**
-   * Retrieves the named audiences that should receive this notification.
+   * Retrieves the list of audiences of the notification representation indicating the named
+   * audiences that should receive this notification.
    *
-   * @return Broader audiences that should receive this notification.
+   * @return The list of audiences of the notification representation indicating the named audiences
+   *     that should receive this notification.
    */
   @XmlElementWrapper(name = "audiences")
   @XmlElement(name = "audience")
@@ -249,6 +367,13 @@ public final class Notification extends Representation {
     return this.audiences;
   }
 
+  /**
+   * Alters the list of audiences of the notification representation indicating the named audiences
+   * that should receive this notification.
+   *
+   * @param audiences The desired list of audiences of the notification representation indicating
+   *     the named audiences that should receive this notification.
+   */
   private void setAudiences(Set<Audience> audiences) {
     this.audiences = audiences;
   }
