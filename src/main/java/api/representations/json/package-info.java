c63606a3eb5b13241c4c255980163ca5bd624626
Jon Freer <freerjm@miamioh.edu>
2018-07-26 00:05:43 -0700

    Introduce package javadoc for api.representations.json package.

---
+++ src/main/java/api/representations/json/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains the constructs that serve as RESTful resource representations that are serialized and
+ * deserialized as the {@code application/json} media type, as well as the types responsible for
+ * creating these representations.
+ */
+package api.representations.json;
