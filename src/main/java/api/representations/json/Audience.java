d4d5bf06f2e42e1e5160f2044aa765a605b2bbb4
Jon Freer <freerjm@miamioh.edu>
2018-07-27 14:04:08 -0700

    Introduce javadoc for audience JSON representation.

--- src/main/java/api/representations/json/Audience.java
+++ src/main/java/api/representations/json/Audience.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Defines the 'application/json' representation of an Audience resource.
+ * Defines the {@code application/json} representation of an Audience resource.
  *
  * @author Jon Freer
  */
@@ -17,41 +17,80 @@ public final class Audience extends Representation {
   private String name;
   private Set<Target> members;
 
+  /** Constructs a new {@link Audience} representation. */
   private Audience(final MediaType mediaType) {
     super(mediaType);
   }
 
+  /**
+   * A builder of {@link Audience} instances.
+   *
+   * @author Jon Freer
+   */
   public static final class Builder extends Representation.Builder {
 
     private UUID uuid;
     private String name;
     private Set<Target> members;
 
+    /** Constructs a builder of {@link Audience} instances. */
     public Builder() {
       super(MediaType.APPLICATION_XML_TYPE);
       this.members = new HashSet<>();
     }
 
+    /**
+     * Sets the universally unique identifier of the {@link Audience} representation being
+     * built.
+     *
+     * @param uuid The desired {@link UUID} of the {@link Audience} representation being built.
+     * @return The updated {@link Audience} builder.
+     */
     public Builder uuid(UUID uuid) {
       this.uuid = uuid;
       return this;
     }
 
+    /**
+     * Sets the name of the {@link Audience} representation being built.
+     *
+     * @param name The desired name of the {@link Audience} representation being built.
+     * @return The updated {@link Target} builder.
+     */
     public Builder name(String name) {
       this.name = name;
       return this;
     }
 
+    /**
+     * Appends the provided {@link Target} to the list of members for the {@link Audience}
+     * representation.
+     *
+     * @param target The {@link Target} to append to the list of members for the {@link
+     *     Audience} representation.
+     * @return The updated {@link Audience} builder.
+     */
     public Builder addMember(Target member) {
       this.members.add(member);
       return this;
     }
 
+    /**
+     * Sets the list of members for the {@link Audience} representation.
+     *
+     * @param members The desired list of members for the {@link Audience} representation.
+     * @return The updated {@link Audience} builder.
+     */
     public Builder members(Set<Target> members) {
       this.members = members;
       return this;
     }
 
+    /**
+     * Builds the {@link Audience} instance.
+     *
+     * @return The {@link Audience} instance.
+     */
     @Override
     public Representation build() {
       Audience a = new Audience(this.mediaType());
@@ -67,40 +106,57 @@ public final class Audience extends Representation {
   }
 
   /**
-   * Retrieves the universally unique identifier for this audience.
+   * Retrieves the universally unique identifier for this audience representation. 
    *
-   * @return The universally unique identifier for this audience.
+   * @return The universally unique identifier for this audience representation. 
    */
   public UUID getUUID() {
     return this.uuid;
   }
 
+  /**
+   * Alters the universally unique identifier of this audience representation.
+   *
+   * @param uuid The desired universally unique identifier of the audience representation.
+   */
   private void setUUID(UUID uuid) {
     this.uuid = uuid;
   }
 
   /**
-   * Retrieves the name of the audience.
+   * Retrieves the name of the audience representation.
    *
-   * @return The name of the audience.
+   * @return The name of the audience representation.
    */
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Alters the name of the audience representation.
+   *
+   * @param name The desired name of the audience representation.
+   */
   private void setName(String name) {
     this.name = name;
   }
 
   /**
-   * Retrieves the members that collectively resemble this audience.
+   * Retrieves the members of the audience representation. 
    *
-   * @return The members that collectively resemble this audience.
+   * @return The members of the audience representation. 
    */
   public Set<Target> getMembers() {
     return this.members;
   }
 
+  /**
+   * Alters the list of members of the audience representation that identify
+   * the individual members within the audience. 
+   *
+   * @return The list of members of the audience representation that identify
+   * the individual members within the audience. 
+   */
   private void setMembers(Set<Target> members) {
     this.members = members;
   }
