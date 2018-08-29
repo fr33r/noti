aed45244d300f01529d8983d9357725b2f70b0d4
Jon Freer <freerjm@miamioh.edu>
2018-08-28 22:38:12 -0700

    Integrate NotificationDataMapper into NotificationRepository.

--- src/main/java/infrastructure/NotificationDataMapper.java
+++ src/main/java/infrastructure/NotificationDataMapper.java
@@ -2,18 +2,21 @@ package infrastructure;
 
 import domain.Audience;
 import domain.EntitySQLFactory;
+import domain.Message;
 import domain.Notification;
 import domain.Target;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import org.slf4j.Logger;
 
-// consider making this package visible only?
 final class NotificationDataMapper extends DataMapper {
 
   private final EntitySQLFactory<Notification, UUID> notificationFactory;
@@ -195,10 +198,172 @@ final class NotificationDataMapper extends DataMapper {
             .append(targetDataMap.getTableAlias())
             .append(".")
             .append(targetDataMap.getColumnNameForField(TargetMetadata.UUID))
-            .append(" WHERE NT.NOTIFICATION_UUID =?;");
+            .append(" WHERE NT.NOTIFICATION_UUID = ?");
     String sql = sb.toString();
     this.logger.debug(sql);
     return sql;
+  }
+
+  private String findNotificationSQL() {
+
+    DataMap notificationDataMap = this.notificationMetadata.getDataMap();
+
+    List<String> columnNames = notificationDataMap.getAllColumnNamesWithAliases();
+    String columns = String.join(", ", columnNames);
+
+    StringBuilder sb =
+        new StringBuilder()
+            .append("SELECT ")
+            .append(columns)
+            .append(" FROM ")
+            .append(notificationDataMap.getTableName())
+            .append(" AS ")
+            .append(notificationDataMap.getTableAlias())
+            .append(" WHERE ")
+            .append(notificationDataMap.getTableAlias())
+            .append(".")
+            .append(notificationDataMap.getColumnNameForField(NotificationMetadata.UUID))
+            .append(" = ?");
+    String sql = sb.toString();
+    this.logger.debug(sql);
+    return sql;
+  }
+
+  private String updateNotificationSQL() {
+
+    DataMap notificationDataMap = this.notificationMetadata.getDataMap();
+
+    List<String> columnNames = notificationDataMap.getAllColumnNamesWithAliases();
+    String columns = String.join(", ", columnNames);
+
+    StringBuilder sb =
+        new StringBuilder()
+            .append("UPDATE ")
+            .append(notificationDataMap.getTableName())
+            .append(" SET ")
+            .append(notificationDataMap.getColumnNameForField(NotificationMetadata.CONTENT))
+            .append(" = ?,")
+            .append(notificationDataMap.getColumnNameForField(NotificationMetadata.STATUS))
+            .append(" = ?,")
+            .append(notificationDataMap.getColumnNameForField(NotificationMetadata.SENT_AT))
+            .append(" = ?,")
+            .append(notificationDataMap.getColumnNameForField(NotificationMetadata.SEND_AT))
+            .append(" = ?")
+            .append(" WHERE ")
+            .append(notificationDataMap.getColumnNameForField(NotificationMetadata.UUID))
+            .append(" = ?");
+    String sql = sb.toString();
+    this.logger.debug(sql);
+    return sql;
+  }
+
+  private String insertNotificationSQL() {
+
+    DataMap notificationDataMap = this.notificationMetadata.getDataMap();
+
+    List<String> columnNames = notificationDataMap.getAllColumnNamesWithAliases();
+    String columns = String.join(", ", columnNames);
+    List<String> placeholderList = new ArrayList<>();
+    for (int i = 0; i < columnNames.size(); i++) {
+      placeholderList.add("?");
+    }
+    String placeholders = String.join(", ", placeholderList);
+
+    StringBuilder sb =
+        new StringBuilder()
+            .append("INSERT INTO ")
+            .append(notificationDataMap.getTableName())
+            .append(" (")
+            .append(columns)
+            .append(") VALUES (")
+            .append(placeholders)
+            .append(")");
+
+    String sql = sb.toString();
+    this.logger.debug(sql);
+    return sql;
+  }
+
+  private String insertMessageSQL() {
+
+    DataMap messageDataMap = this.messageMetadata.getDataMap();
+
+    List<String> columnNames = messageDataMap.getAllColumnNamesWithAliases();
+    String columns = String.join(", ", columnNames);
+    List<String> placeholderList = new ArrayList<>();
+    for (int i = 0; i < columnNames.size(); i++) {
+      placeholderList.add("?");
+    }
+    String placeholders = String.join(", ", placeholderList);
+
+    StringBuilder sb =
+        new StringBuilder()
+            .append("INSERT INTO ")
+            .append(messageDataMap.getTableName())
+            .append(" (")
+            .append(columns)
+            .append(") VALUES (")
+            .append(placeholders)
+            .append(")");
+
+    String sql = sb.toString();
+    this.logger.debug(sql);
+    return sql;
+  }
+
+  private String associateTargetSQL() {
+    return "INSERT INTO NOTIFICATION_TARGET (NOTIFICATION_UUID, TARGET_UUID) VALUES (?, ?)";
+  }
+
+  private String associateAudienceSQL() {
+    return "INSERT INTO NOTIFICATION_AUDIENCE (NOTIFICATION_UUID, AUDIENCE_UUID) VALUES (?, ?)";
+  }
+
+  private String deleteNotificationSQL() {
+
+    DataMap notificationDataMap = this.notificationMetadata.getDataMap();
+
+    List<String> columnNames = notificationDataMap.getAllColumnNamesWithAliases();
+    String columns = String.join(", ", columnNames);
+
+    StringBuilder sb =
+        new StringBuilder()
+            .append("DELETE FROM ")
+            .append(notificationDataMap.getTableName())
+            .append(" WHERE ")
+            .append(notificationDataMap.getColumnNameForField(NotificationMetadata.UUID))
+            .append(" = ?");
+
+    String sql = sb.toString();
+    this.logger.debug(sql);
+    return sql;
+  }
+
+  private String deleteMessagesSQL() {
+
+    DataMap messageDataMap = this.messageMetadata.getDataMap();
+
+    List<String> columnNames = messageDataMap.getAllColumnNamesWithAliases();
+    String columns = String.join(", ", columnNames);
+
+    StringBuilder sb =
+        new StringBuilder()
+            .append("DELETE FROM ")
+            .append(messageDataMap.getTableName())
+            .append(" WHERE ")
+            .append("NOTIFICATION_UUID = ?");
+
+    String sql = sb.toString();
+    this.logger.debug(sql);
+    return sql;
+  }
+
+  private String dissociateTargetSQL() {
+    return "DELETE FROM NOTIFICATION_TARGET WHERE NOTIFICATION_UUID = ?";
+  }
+
+  private String dissociateAudienceSQL() {
+    return "DELETE FROM NOTIFICATION_AUDIENCE WHERE NOTIFICATION_UUID = ?";
   }
 
   Set<Notification> find(
@@ -257,6 +422,188 @@ final class NotificationDataMapper extends DataMapper {
         }
       }
       return notifications;
+    } catch (SQLException x) {
+      throw new RuntimeException(x);
+    }
+  }
+
+  Notification find(final UUID uuid) {
+
+    // define SQL.
+    String notificationSQL = this.findNotificationSQL();
+    String messagesSQL = this.findMessagesSQL();
+    String audiencesSQL = this.findAudiencesSQL();
+    String recipientsSQL = this.findRecipientsSQL();
+    String audienceMembersSQL = this.findAudienceMembersSQL();
+
+    Notification notification = null;
+
+    // find matching notification.
+    try (final PreparedStatement getNotificationStatement =
+            this.getUnitOfWork().createPreparedStatement(notificationSQL);
+        final PreparedStatement getTargetsStatement =
+            this.getUnitOfWork().createPreparedStatement(recipientsSQL);
+        final PreparedStatement getMessagesStatement =
+            this.getUnitOfWork().createPreparedStatement(messagesSQL);
+        final PreparedStatement getAudiencesStatement =
+            this.getUnitOfWork().createPreparedStatement(audiencesSQL);
+        final PreparedStatement getAudienceMembersStatement =
+            this.getUnitOfWork().createPreparedStatement(audienceMembersSQL)) {
+
+      int index = 1;
+      getNotificationStatement.setString(index, uuid.toString());
+      getTargetsStatement.setString(index, uuid.toString());
+      getMessagesStatement.setString(index, uuid.toString());
+      getAudiencesStatement.setString(index, uuid.toString());
+      getAudienceMembersStatement.setString(index, uuid.toString());
+
+      try (final ResultSet notificationRS = getNotificationStatement.executeQuery();
+          final ResultSet targetsRS = getTargetsStatement.executeQuery();
+          final ResultSet messagesRS = getMessagesStatement.executeQuery();
+          final ResultSet audiencesRS = getAudiencesStatement.executeQuery();
+          final ResultSet membersRS = getAudienceMembersStatement.executeQuery()) {
+
+        if (notificationRS.next()) {
+          notification =
+              this.notificationFactory.reconstitute(
+                  notificationRS, targetsRS, messagesRS, audiencesRS, membersRS);
+        }
+      }
+      return notification;
+    } catch (SQLException x) {
+      throw new RuntimeException(x);
+    }
+  }
+
+  void insert(final Notification notification) {
+
+    final String notificationSQL = this.insertNotificationSQL();
+    final String associateTargetSQL = this.associateTargetSQL();
+    final String messageSQL = this.insertMessageSQL();
+    final String associateAudienceSQL = this.associateAudienceSQL();
+
+    try (final PreparedStatement createNotificationStatement =
+        this.getUnitOfWork().createPreparedStatement(notificationSQL)) {
+      int index = 0;
+      createNotificationStatement.setString(++index, notification.getId().toString());
+      createNotificationStatement.setString(++index, notification.content());
+      createNotificationStatement.setString(++index, notification.status().toString());
+
+      if (notification.sendAt() != null) {
+        createNotificationStatement.setTimestamp(
+            ++index, new Timestamp(notification.sendAt().getTime()));
+      } else {
+        createNotificationStatement.setNull(++index, Types.TIMESTAMP);
+      }
+
+      if (notification.sentAt() != null) {
+        createNotificationStatement.setTimestamp(
+            ++index, new Timestamp(notification.sentAt().getTime()));
+      } else {
+        createNotificationStatement.setNull(++index, Types.TIMESTAMP);
+      }
+
+      createNotificationStatement.executeUpdate();
+
+      for (Target target : notification.directRecipients()) {
+        try (final PreparedStatement associateTargetStatement =
+            this.getUnitOfWork().createPreparedStatement(associateTargetSQL)) {
+          index = 0;
+          associateTargetStatement.setString(++index, notification.getId().toString());
+          associateTargetStatement.setString(++index, target.getId().toString());
+          associateTargetStatement.executeUpdate();
+        }
+      }
+
+      for (Audience audience : notification.audiences()) {
+        try (final PreparedStatement associateAudienceStatement =
+            this.getUnitOfWork().createPreparedStatement(associateAudienceSQL)) {
+          index = 0;
+          associateAudienceStatement.setString(++index, notification.getId().toString());
+          associateAudienceStatement.setString(++index, audience.getId().toString());
+          associateAudienceStatement.executeUpdate();
+        }
+      }
+
+      for (Message message : notification.messages()) {
+        try (final PreparedStatement createMessageStatement =
+            this.getUnitOfWork().createPreparedStatement(messageSQL)) {
+          index = 0;
+          createMessageStatement.setInt(++index, message.getId());
+          createMessageStatement.setString(++index, message.getFrom().toE164());
+          createMessageStatement.setString(++index, message.getTo().toE164());
+          createMessageStatement.setString(++index, message.getContent());
+          createMessageStatement.setString(++index, message.getStatus().toString());
+          createMessageStatement.setString(++index, notification.getId().toString());
+          createMessageStatement.setString(++index, message.getExternalId());
+          createMessageStatement.executeUpdate();
+        }
+      }
+
+    } catch (SQLException x) {
+      throw new RuntimeException(x);
+    }
+  }
+
+  void update(final Notification notification) {
+
+    String notificationSQL = this.updateNotificationSQL();
+    try (final PreparedStatement updateNotificationStatement =
+        this.getUnitOfWork().createPreparedStatement(notificationSQL)) {
+      int index = 0;
+      updateNotificationStatement.setString(++index, notification.content());
+      updateNotificationStatement.setString(++index, notification.status().toString());
+
+      if (notification.sentAt() == null) {
+        updateNotificationStatement.setNull(++index, Types.TIMESTAMP);
+      } else {
+        updateNotificationStatement.setTimestamp(
+            ++index, new Timestamp(notification.sentAt().getTime()));
+      }
+
+      if (notification.sendAt() == null) {
+        updateNotificationStatement.setNull(++index, Types.TIMESTAMP);
+      } else {
+        updateNotificationStatement.setTimestamp(
+            ++index, new Timestamp(notification.sendAt().getTime()));
+      }
+
+      updateNotificationStatement.setString(++index, notification.getId().toString());
+
+      updateNotificationStatement.executeUpdate();
+    } catch (SQLException x) {
+      throw new RuntimeException(x);
+    }
+  }
+
+  void delete(final UUID uuid) {
+
+    final String deleteNotificationSQL = this.deleteNotificationSQL();
+    final String deleteMessagesSQL = this.deleteMessagesSQL();
+    final String deleteTargetAssociationsSQL = this.dissociateTargetSQL();
+    final String deleteAudienceAssociationsSQL = this.dissociateAudienceSQL();
+
+    try (final PreparedStatement deleteMessagesStatement =
+            this.getUnitOfWork().createPreparedStatement(deleteMessagesSQL);
+        final PreparedStatement deleteNotificationStatement =
+            this.getUnitOfWork().createPreparedStatement(deleteNotificationSQL);
+        final PreparedStatement deleteTargetAssociationsStatement =
+            this.getUnitOfWork().createPreparedStatement(deleteTargetAssociationsSQL);
+        final PreparedStatement deleteAudienceAssociationsStatement =
+            this.getUnitOfWork().createPreparedStatement(deleteAudienceAssociationsSQL)) {
+
+      deleteMessagesStatement.setString(1, uuid.toString());
+      deleteMessagesStatement.executeUpdate();
+
+      deleteTargetAssociationsStatement.setString(1, uuid.toString());
+      deleteTargetAssociationsStatement.executeUpdate();
+
+      deleteAudienceAssociationsStatement.setString(1, uuid.toString());
+      deleteAudienceAssociationsStatement.executeUpdate();
+
+      deleteNotificationStatement.setString(1, uuid.toString());
+      deleteNotificationStatement.executeUpdate();
+
     } catch (SQLException x) {
       throw new RuntimeException(x);
     }
--- src/main/java/infrastructure/NotificationRepository.java
+++ src/main/java/infrastructure/NotificationRepository.java
@@ -1,18 +1,10 @@
 package infrastructure;
 
-import domain.Audience;
 import domain.EntitySQLFactory;
-import domain.Message;
 import domain.Notification;
-import domain.Target;
 import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.sql.Types;
 import java.util.Set;
 import java.util.UUID;
 
@@ -57,7 +49,16 @@ public final class NotificationRepository extends SQLRepository
    */
   @Override
   public Set<Notification> get(final Query<Notification> query) {
-    return query.execute();
+    final Span span =
+        this.tracer
+            .buildSpan("NotificationRepostory#get(query)")
+            .asChildOf(this.tracer.activeSpan())
+            .start();
+    try (final Scope scope = this.tracer.scopeManager().activate(span, false)) {
+      return query.execute();
+    } finally {
+      span.finish();
+    }
   }
 
   /**
@@ -70,52 +71,11 @@ public final class NotificationRepository extends SQLRepository
   public Notification get(final UUID uuid) {
     final Span span =
         this.tracer
-            .buildSpan("NotificationRepostory#get")
+            .buildSpan("NotificationRepostory#get(uuid)")
             .asChildOf(this.tracer.activeSpan())
             .start();
-    Notification notification = null;
-    final String notificationSQL = "SELECT N.* FROM NOTIFICATION AS N WHERE N.UUID = ?;";
-    final String targetSQL =
-        "SELECT T.* FROM TARGET AS T INNER JOIN NOTIFICATION_TARGET AS NT ON T.UUID = NT.TARGET_UUID WHERE NT.NOTIFICATION_UUID = ?";
-    final String messageSQL = "SELECT M.* FROM MESSAGE AS M WHERE M.NOTIFICATION_UUID = ?;";
-    final String audiencesSQL =
-        "SELECT A.* FROM AUDIENCE AS A INNER JOIN NOTIFICATION_AUDIENCE AS NA ON A.UUID = NA.AUDIENCE_UUID WHERE NA.NOTIFICATION_UUID = ?";
-    final String audienceMembersSQL =
-        "SELECT AT.AUDIENCE_UUID, T.* FROM ("
-            + audiencesSQL
-            + ") AS AUDIENCES INNER JOIN AUDIENCE_TARGET AS AT ON AUDIENCES.UUID = AT.AUDIENCE_UUID INNER JOIN TARGET AS T ON AT.TARGET_UUID = T.UUID;";
-    try (final Scope scope = this.tracer.scopeManager().activate(span, false);
-        final PreparedStatement getNotificationStatement =
-            this.getUnitOfWork().createPreparedStatement(notificationSQL);
-        final PreparedStatement getTargetsStatement =
-            this.getUnitOfWork().createPreparedStatement(targetSQL);
-        final PreparedStatement getMessagesStatement =
-            this.getUnitOfWork().createPreparedStatement(messageSQL);
-        final PreparedStatement getAudiencesStatement =
-            this.getUnitOfWork().createPreparedStatement(audiencesSQL);
-        final PreparedStatement getAudienceMembersStatement =
-            this.getUnitOfWork().createPreparedStatement(audienceMembersSQL)) {
-      getNotificationStatement.setString(1, uuid.toString());
-      getTargetsStatement.setString(1, uuid.toString());
-      getMessagesStatement.setString(1, uuid.toString());
-      getAudiencesStatement.setString(1, uuid.toString());
-      getAudienceMembersStatement.setString(1, uuid.toString());
-
-      try (final ResultSet notificationRS = getNotificationStatement.executeQuery();
-          final ResultSet targetsRS = getTargetsStatement.executeQuery();
-          final ResultSet messagesRS = getMessagesStatement.executeQuery();
-          final ResultSet audiencesRS = getAudiencesStatement.executeQuery();
-          final ResultSet membersRS = getAudienceMembersStatement.executeQuery()) {
-
-        if (notificationRS.next()) {
-          notification =
-              this.notificationFactory.reconstitute(
-                  notificationRS, targetsRS, messagesRS, audiencesRS, membersRS);
-        }
-      }
-      return notification;
-    } catch (SQLException x) {
-      throw new RuntimeException(x);
+    try (final Scope scope = this.tracer.scopeManager().activate(span, false)) {
+      return this.notificationDataMapper.find(uuid);
     } finally {
       span.finish();
     }
@@ -135,37 +95,11 @@ public final class NotificationRepository extends SQLRepository
             .buildSpan("NotificationRepository#put")
             .asChildOf(this.tracer.activeSpan())
             .start();
-    final String sql =
-        "UPDATE NOTIFICATION SET MESSAGE = ?, STATUS = ?, SENT_AT = ?, SEND_AT = ? WHERE UUID = ?;";
     try (final Scope scope = this.tracer.scopeManager().activate(span, false)) {
       if (this.get(notification.getId()) == null) {
         this.add(notification);
       } else {
-        try (final PreparedStatement replaceNotificationStatement =
-            this.getUnitOfWork().createPreparedStatement(sql)) {
-          replaceNotificationStatement.setString(1, notification.content());
-          replaceNotificationStatement.setString(2, notification.status().toString());
-
-          if (notification.sentAt() == null) {
-            replaceNotificationStatement.setNull(3, Types.TIMESTAMP);
-          } else {
-            replaceNotificationStatement.setTimestamp(
-                3, new Timestamp(notification.sentAt().getTime()));
-          }
-
-          if (notification.sendAt() == null) {
-            replaceNotificationStatement.setNull(4, Types.TIMESTAMP);
-          } else {
-            replaceNotificationStatement.setTimestamp(
-                4, new Timestamp(notification.sendAt().getTime()));
-          }
-
-          replaceNotificationStatement.setString(5, notification.getId().toString());
-
-          replaceNotificationStatement.executeUpdate();
-        } catch (SQLException x) {
-          throw new RuntimeException(x);
-        }
+        this.notificationDataMapper.update(notification);
       }
     } finally {
       span.finish();
@@ -184,70 +118,8 @@ public final class NotificationRepository extends SQLRepository
             .buildSpan("NotificationRepository#add")
             .asChildOf(this.tracer.activeSpan())
             .start();
-    final String notificationSQL =
-        "INSERT INTO NOTIFICATION (UUID, MESSAGE, STATUS, SEND_AT, SENT_AT) VALUES (?, ?, ?, ?, ?);";
-    final String associateTargetSQL =
-        "INSERT INTO NOTIFICATION_TARGET (NOTIFICATION_UUID, TARGET_UUID) VALUES (?, ?);";
-    final String messageSQL =
-        "INSERT INTO MESSAGE (ID, `FROM`, `TO`, CONTENT, STATUS, NOTIFICATION_UUID, EXTERNAL_ID) VALUES (?, ?, ?, ?, ?, ?, ?);";
-    final String associateAudienceSQL =
-        "INSERT INTO NOTIFICATION_AUDIENCE (NOTIFICATION_UUID, AUDIENCE_UUID) VALUES (?, ?);";
-
-    try (final Scope scope = this.tracer.scopeManager().activate(span, false);
-        final PreparedStatement createNotificationStatement =
-            this.getUnitOfWork().createPreparedStatement(notificationSQL)) {
-      createNotificationStatement.setString(1, notification.getId().toString());
-      createNotificationStatement.setString(2, notification.content());
-      createNotificationStatement.setString(3, notification.status().toString());
-
-      if (notification.sendAt() != null) {
-        createNotificationStatement.setTimestamp(4, new Timestamp(notification.sendAt().getTime()));
-      } else {
-        createNotificationStatement.setNull(4, Types.TIMESTAMP);
-      }
-
-      if (notification.sentAt() != null) {
-        createNotificationStatement.setTimestamp(5, new Timestamp(notification.sentAt().getTime()));
-      } else {
-        createNotificationStatement.setNull(5, Types.TIMESTAMP);
-      }
-
-      createNotificationStatement.executeUpdate();
-
-      for (Target target : notification.directRecipients()) {
-        try (final PreparedStatement associateTargetStatement =
-            this.getUnitOfWork().createPreparedStatement(associateTargetSQL)) {
-          associateTargetStatement.setString(1, notification.getId().toString());
-          associateTargetStatement.setString(2, target.getId().toString());
-          associateTargetStatement.executeUpdate();
-        }
-      }
-
-      for (Audience audience : notification.audiences()) {
-        try (final PreparedStatement associateAudienceStatement =
-            this.getUnitOfWork().createPreparedStatement(associateAudienceSQL)) {
-          associateAudienceStatement.setString(1, notification.getId().toString());
-          associateAudienceStatement.setString(2, audience.getId().toString());
-          associateAudienceStatement.executeUpdate();
-        }
-      }
-
-      for (Message message : notification.messages()) {
-        try (final PreparedStatement createMessageStatement =
-            this.getUnitOfWork().createPreparedStatement(messageSQL)) {
-          createMessageStatement.setInt(1, message.getId());
-          createMessageStatement.setString(2, message.getFrom().toE164());
-          createMessageStatement.setString(3, message.getTo().toE164());
-          createMessageStatement.setString(4, message.getContent());
-          createMessageStatement.setString(5, message.getStatus().toString());
-          createMessageStatement.setString(6, notification.getId().toString());
-          createMessageStatement.setString(7, message.getExternalId());
-          createMessageStatement.executeUpdate();
-        }
-      }
-
-    } catch (SQLException x) {
-      throw new RuntimeException(x);
+    try (final Scope scope = this.tracer.scopeManager().activate(span, false)) {
+      this.notificationDataMapper.insert(notification);
     } finally {
       span.finish();
     }
@@ -265,35 +137,8 @@ public final class NotificationRepository extends SQLRepository
             .buildSpan("NotificationRepository#remove")
             .asChildOf(this.tracer.activeSpan())
             .start();
-    final String deleteNotificationSQL = "DELETE FROM NOTIFICATION WHERE UUID = ?;";
-    final String deleteMessagesSQL = "DELETE FROM MESSAGE WHERE NOTIFICATION_UUID = ?;";
-    final String deleteTargetAssociationsSQL =
-        "DELETE FROM NOTIFICATION_TARGET WHERE NOTIFICATION_UUID = ?;";
-    final String deleteAudienceAssociationsSQL =
-        "DELETE FROM NOTIFICATION_AUDIENCE WHERE NOTIFICATION_UUID = ?;";
-    try (final Scope scope = this.tracer.scopeManager().activate(span, false);
-        final PreparedStatement deleteMessagesStatement =
-            this.getUnitOfWork().createPreparedStatement(deleteMessagesSQL);
-        final PreparedStatement deleteNotificationStatement =
-            this.getUnitOfWork().createPreparedStatement(deleteNotificationSQL);
-        final PreparedStatement deleteTargetAssociationsStatement =
-            this.getUnitOfWork().createPreparedStatement(deleteTargetAssociationsSQL);
-        final PreparedStatement deleteAudienceAssociationsStatement =
-            this.getUnitOfWork().createPreparedStatement(deleteAudienceAssociationsSQL)) {
-      deleteMessagesStatement.setString(1, uuid.toString());
-      deleteMessagesStatement.executeUpdate();
-
-      deleteTargetAssociationsStatement.setString(1, uuid.toString());
-      deleteTargetAssociationsStatement.executeUpdate();
-
-      deleteAudienceAssociationsStatement.setString(1, uuid.toString());
-      deleteAudienceAssociationsStatement.executeUpdate();
-
-      deleteNotificationStatement.setString(1, uuid.toString());
-      deleteNotificationStatement.executeUpdate();
-
-    } catch (SQLException x) {
-      throw new RuntimeException(x);
+    try (final Scope scope = this.tracer.scopeManager().activate(span, false)) {
+      this.notificationDataMapper.delete(uuid);
     } finally {
       span.finish();
     }
