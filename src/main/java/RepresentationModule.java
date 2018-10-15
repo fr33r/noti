146f3f52fdc3c91824965debbe52903d7f2cc282
Jon Freer <freerjm@miamioh.edu>
2018-10-14 22:35:00 -0700

    Introduce support for YAML representations.

--- src/main/java/RepresentationModule.java
+++ src/main/java/RepresentationModule.java
@@ -2,6 +2,9 @@ import api.representations.RepresentationFactory;
 import api.representations.json.JSONRepresentationFactory;
 import api.representations.siren.SirenRepresentationFactory;
 import api.representations.xml.XMLRepresentationFactory;
+import api.representations.yaml.YAMLRepresentationFactory;
+import com.fasterxml.jackson.jaxrs.yaml.JacksonYAMLProvider;
+import com.fasterxml.jackson.jaxrs.yaml.YAMLMediaTypes;
 import configuration.NotiConfiguration;
 import io.dropwizard.setup.Environment;
 import io.opentracing.util.GlobalTracer;
@@ -16,6 +19,9 @@ public final class RepresentationModule extends NotiModule {
   private static final String JSON_REPRESENTATION_FACTORY = "JSONRepresentationFactory";
   private static final String XML_REPRESENTATION_FACTORY = "XMLRepresentationFactory";
   private static final String SIREN_REPRESENTATION_FACTORY = "SirenRepresentationFactory";
+  private static final MediaType APPLICATION_X_YAML_TYPE = new MediaType("application", "x-yaml");
+  private static final MediaType TEXT_X_YAML_TYPE = new MediaType("text", "x-yaml");
+  private static final MediaType TEXT_VND_TAML_TYPE = new MediaType("text", "vnd.yaml");
 
   public RepresentationModule(NotiConfiguration configuration, Environment environment) {
     super(configuration, environment);
@@ -31,11 +37,19 @@ public final class RepresentationModule extends NotiModule {
         new XMLRepresentationFactory(GlobalTracer.get());
     RepresentationFactory sirenRepresentationFactory =
         new SirenRepresentationFactory(GlobalTracer.get());
+    RepresentationFactory yamlRepresentationFactory =
+        new YAMLRepresentationFactory(GlobalTracer.get());
 
     representationIndustry.put(jsonRepresentationFactory.getMediaType(), jsonRepresentationFactory);
     representationIndustry.put(xmlRepresentationFactory.getMediaType(), xmlRepresentationFactory);
     representationIndustry.put(
         sirenRepresentationFactory.getMediaType(), sirenRepresentationFactory);
+    representationIndustry.put(
+        YAMLMediaTypes.APPLICATION_JACKSON_YAML_TYPE, yamlRepresentationFactory);
+    representationIndustry.put(YAMLMediaTypes.TEXT_JACKSON_YAML_TYPE, yamlRepresentationFactory);
+    representationIndustry.put(APPLICATION_X_YAML_TYPE, yamlRepresentationFactory);
+    representationIndustry.put(TEXT_X_YAML_TYPE, yamlRepresentationFactory);
+    representationIndustry.put(TEXT_VND_TAML_TYPE, yamlRepresentationFactory);
 
     // register representation factories with environment.
     this.getEnvironment()
@@ -57,5 +71,7 @@ public final class RepresentationModule extends NotiModule {
                     .to(RepresentationFactory.class);
               }
             });
+
+    this.getEnvironment().jersey().register(JacksonYAMLProvider.class);
   }
 }
---
+++ src/main/java/api/representations/yaml/Audience.java
@@ -0,0 +1,217 @@
+package api.representations.yaml;
+
+import api.representations.Representation;
+import com.fasterxml.jackson.jaxrs.yaml.YAMLMediaTypes;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Defines the {@code application/json} representation of an Audience resource.
+ *
+ * @author Jon Freer
+ */
+public final class Audience extends Representation {
+
+  private UUID uuid;
+  private String name;
+  private Set<Target> members;
+
+  /** Constructs a new {@link Audience} representation. */
+  private Audience() {
+    super(YAMLMediaTypes.APPLICATION_JACKSON_YAML_TYPE);
+  }
+
+  /**
+   * A builder of {@link Audience} instances.
+   *
+   * @author Jon Freer
+   */
+  public static final class Builder extends Representation.Builder {
+
+    private UUID uuid;
+    private String name;
+    private Set<Target> members;
+
+    /** Constructs a builder of {@link Audience} instances. */
+    public Builder() {
+      super(YAMLMediaTypes.APPLICATION_JACKSON_YAML_TYPE);
+      this.members = new HashSet<>();
+    }
+
+    /**
+     * Sets the universally unique identifier of the {@link Audience} representation being built.
+     *
+     * @param uuid The desired {@link UUID} of the {@link Audience} representation being built.
+     * @return The updated {@link Audience} builder.
+     */
+    public Builder uuid(UUID uuid) {
+      this.uuid = uuid;
+      return this;
+    }
+
+    /**
+     * Sets the name of the {@link Audience} representation being built.
+     *
+     * @param name The desired name of the {@link Audience} representation being built.
+     * @return The updated {@link Target} builder.
+     */
+    public Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    /**
+     * Appends the provided {@link Target} to the list of members for the {@link Audience}
+     * representation.
+     *
+     * @param member The {@link Target} to append to the list of members for the {@link Audience}
+     *     representation.
+     * @return The updated {@link Audience} builder.
+     */
+    public Builder addMember(Target member) {
+      this.members.add(member);
+      return this;
+    }
+
+    /**
+     * Sets the list of members for the {@link Audience} representation.
+     *
+     * @param members The desired list of members for the {@link Audience} representation.
+     * @return The updated {@link Audience} builder.
+     */
+    public Builder members(Set<Target> members) {
+      this.members = members;
+      return this;
+    }
+
+    /**
+     * Builds the {@link Audience} instance.
+     *
+     * @return The {@link Audience} instance.
+     */
+    @Override
+    public Representation build() {
+      Audience a = new Audience();
+      a.setLocation(this.location());
+      a.setEncoding(this.encoding());
+      a.setLanguage(this.language());
+      a.setLastModified(this.lastModified());
+      a.setUUID(this.uuid);
+      a.setName(this.name);
+      a.setMembers(this.members);
+      return a;
+    }
+  }
+
+  /**
+   * Retrieves the universally unique identifier for this audience representation.
+   *
+   * @return The universally unique identifier for this audience representation.
+   */
+  public UUID getUUID() {
+    return this.uuid;
+  }
+
+  /**
+   * Alters the universally unique identifier of this audience representation.
+   *
+   * @param uuid The desired universally unique identifier of the audience representation.
+   */
+  private void setUUID(UUID uuid) {
+    this.uuid = uuid;
+  }
+
+  /**
+   * Retrieves the name of the audience representation.
+   *
+   * @return The name of the audience representation.
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Alters the name of the audience representation.
+   *
+   * @param name The desired name of the audience representation.
+   */
+  private void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Retrieves the members of the audience representation.
+   *
+   * @return The members of the audience representation.
+   */
+  public Set<Target> getMembers() {
+    return this.members;
+  }
+
+  /**
+   * Alters the list of members of the audience representation that identify the individual members
+   * within the audience.
+   *
+   * @return The list of members of the audience representation that identify the individual members
+   *     within the audience.
+   */
+  private void setMembers(Set<Target> members) {
+    this.members = members;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @param obj {@inheritDoc}
+   * @return {@inheritDoc}
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null || this.getClass() != obj.getClass()) return false;
+
+    Audience audience = (Audience) obj;
+    boolean sameUUID =
+        this.getUUID() == null && audience.getUUID() == null
+            || this.getUUID() != null
+                && audience.getUUID() != null
+                && this.getUUID().equals(audience.getUUID());
+    boolean sameName =
+        this.getName() == null && audience.getName() == null
+            || this.getName() != null
+                && audience.getName() != null
+                && this.name.equals(audience.getName());
+    boolean sameMembers =
+        this.getMembers() == null && audience.getMembers() == null
+            || this.getMembers() != null
+                && audience.getMembers() != null
+                && this.getMembers().equals(audience.getMembers());
+
+    return sameUUID && sameName && sameMembers;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @return {@inheritDoc}
+   */
+  @Override
+  public int hashCode() {
+    int hashCode = 1;
+    final int prime = 17;
+
+    if (this.getUUID() != null) {
+      hashCode = hashCode * prime + this.getUUID().hashCode();
+    }
+
+    if (this.getName() != null) {
+      hashCode = hashCode * prime + this.getName().hashCode();
+    }
+
+    if (this.getMembers() != null) {
+      hashCode = hashCode * prime + this.getMembers().hashCode();
+    }
+
+    return hashCode;
+  }
+}
